94a0b192ea6c0a98f6b4a000ffc7f38800d7b71f
Aji Indrayana <ajivancouver@gmail.com>
2017-10-10 22:06:06 -0700

    save vehicles data changes to db

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using vega.Controllers.Resources;
 using vega.Models;
+using vega.Persistence;
 
 namespace vega.Controllers
 {
@@ -10,15 +12,21 @@ namespace vega.Controllers
     {
         // to use automapper we need to inject IMapper interface to constructor
         private readonly IMapper mapper;
-        public VehiclesController(IMapper mapper)
+        // inject dbContext to VehicleController
+        private readonly VegaDbContext context;
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
+            this.context = context;
             this.mapper = mapper;
         }
         [HttpPost]
-        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
             // map api resource to domain object
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            // add to context and save the changes
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
             return Ok(vehicle);
         }
     }
